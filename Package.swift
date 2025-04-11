9fd200759a8eff4537877b58b1f1684fad6fe227
Dejan Atanasov <39065925+atanasovdejan@users.noreply.github.com>
2025-04-12 00:03:03 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "RangeSeekSlider"
+    name: "RangeSeekSlider",
+    products: [
+        .library(
+            name: "RangeSeekSlider",
+            targets: ["RangeSeekSlider"]),
+    ],
+    dependencies: [],
+    targets:[
+        .target(
+            name: "RangeSeekSlider",
+            path: "Sources"
+        )
+    ]
 )
